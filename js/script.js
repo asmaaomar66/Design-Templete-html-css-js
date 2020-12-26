ec93726d8137806f8a146632e8cb42e7a12ca342
AsmaaOmar <62079548+asmaaomar66@users.noreply.github.com>
2020-12-26 20:56:12 +0200

    header section
    header section

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function HoverButton (){
+    document.getElementById("btn").style.backgroundColor = "#96dac8"
+}
+function Hover (){
+    document.getElementById("btn").style.backgroundColor = "#00BC8A"
+}
