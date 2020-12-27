4ecc3812e9da9cb2351c01220d0ed7e2c6e8151b
AsmaaOmar <62079548+asmaaomar66@users.noreply.github.com>
2020-12-27 04:34:21 +0200

    second and third section
    second and third section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 function HoverButton (){
-    document.getElementById("btn").style.backgroundColor = "#96dac8"
+    document.getElementById("btn").style.backgroundColor = "#96dac8" ;
 }
 function Hover (){
-    document.getElementById("btn").style.backgroundColor = "#00BC8A"
+    document.getElementById("btn").style.backgroundColor = "#00BC8A" ; 
+}
+function show() {
+    document.getElementById("btn1").style.visibility = "visible" ;
+}
+function hide() {
+    document.getElementById("btn1").style.visibility = "hidden" ;
+}
+var n = 0 ; 
+var v = 0 ; 
+var r = 0 ; 
+
+function Hovered () {
+    n = n+1 ; 
+    document.getElementById("img1").innerHTML = n ;
+}
+function Hover () {
+    v = v+1 ; 
+    document.getElementById("img2").innerHTML = v ;
+}
+function Hove () {
+    r = r+1 ; 
+    document.getElementById("img3").innerHTML = r ;
 }
